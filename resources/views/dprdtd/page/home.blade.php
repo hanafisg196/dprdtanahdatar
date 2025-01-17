15a41c21dc22372d74900596636d300808b6947e
hanafisg196 <hanafisg196@gmail.com>
2025-01-17 10:10:25 +0700

    sdfdfsd

--- resources/views/dprdtd/page/home.blade.php
+++ resources/views/dprdtd/page/home.blade.php
@@ -103,13 +103,125 @@
             </a>
         </div>
     </div>
-    
 </div>
 <div class="container">
     <div class="row row-eq-height">
-        <div class="col-md-9 md-padding main-content">
-           
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    
+                    <div class="filter-by style2">
+                        <ul id="filters">
+                            <li class="selected"><a href="#" class="filter" data-filter="*"><span>Show All</span></a></li>
+                            <li><a href="#" class="filter" data-filter=".design"><span>Web Design</span></a></li>
+                            <li><a href="#" class="filter" data-filter=".develop"><span>Web Development</span></a></li>
+                            <li><a href="#" class="filter" data-filter=".computers"><span>Computers</span></a></li>
+                        </ul>
+                    </div>
+                    
+                    <div class="portfolio grid p-3-cols p-style1" id="grid">
+                            <div class="portfolio-item design">
+                                <figure>
+                                    <div class="port-img">
+                                        <div class="icon-links">
+                                            <a href="portfolio-single.html" class="link white-bg"><i class="fa fa-link"></i></a><a href="assets/images/portfolio/grid/1.jpg" class="zoom main-bg" title="Quality Products for Companies"><i class="fa fa-search-plus"></i></a>
+                                        </div>
+                                        <img alt="" src="assets/images/portfolio/grid/1.jpg">
+                                    </div>
+                                    <figcaption>
+                                        <div class="port-captions">
+                                            <h4 class="uppercase"><a class="main-color" href="portfolio-single.html">Quality Products for Companies</a></h4>
+                                            <p class="description"><a href="#">Design</a>, <a href="#">Development</a></p>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <div class="portfolio-item develop">
+                                <figure>
+                                    <div class="port-img">
+                                        <div class="icon-links">
+                                            <a href="portfolio-single.html" class="link white-bg"><i class="fa fa-link"></i></a><a href="assets/images/portfolio/grid/2.jpg" class="zoom main-bg" title="Nature vs. Man"><i class="fa fa-search-plus"></i></a>
+                                        </div>
+                                        <img alt="" src="assets/images/portfolio/grid/2.jpg">
+                                    </div>
+                                    <figcaption>
+                                        <div class="port-captions">
+                                            <h4 class="uppercase"><a class="main-color" href="portfolio-single.html">Nature vs. Man</a></h4>
+                                            <p class="description"><a href="#">Design</a>, <a href="#">Development</a></p>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <div class="portfolio-item computers">
+                                <figure>
+                                    <div class="port-img">
+                                        <div class="icon-links">
+                                            <a href="portfolio-single.html" class="link white-bg"><i class="fa fa-link"></i></a><a href="assets/images/portfolio/grid/3.jpg" class="zoom main-bg" title="A Day with Sunshine & Bliss"><i class="fa fa-search-plus"></i></a>
+                                        </div>
+                                        <img alt="" src="assets/images/portfolio/grid/3.jpg">
+                                    </div>
+                                    <figcaption>
+                                        <div class="port-captions">
+                                            <h4 class="uppercase"><a class="main-color" href="portfolio-single.html">A Day with Sunshine & Bliss</a></h4>
+                                            <p class="description"><a href="#">Design</a>, <a href="#">Development</a></p>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <div class="portfolio-item develop">
+                                <figure>
+                                    <div class="port-img">
+                                        <div class="icon-links">
+                                            <a href="portfolio-single.html" class="link white-bg"><i class="fa fa-link"></i></a><a href="assets/images/portfolio/grid/4.jpg" class="zoom main-bg" title="A Workplace for Champions"><i class="fa fa-search-plus"></i></a>
+                                        </div>
+                                        <img alt="" src="assets/images/portfolio/grid/4.jpg">
+                                    </div>
+                                    <figcaption>
+                                        <div class="port-captions">
+                                            <h4 class="uppercase"><a class="main-color" href="portfolio-single.html">A Workplace for Champions</a></h4>
+                                            <p class="description"><a href="#">Design</a>, <a href="#">Development</a></p>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <div class="portfolio-item design">
+                                <figure>
+                                    <div class="port-img">
+                                        <div class="icon-links">
+                                            <a href="portfolio-single.html" class="link white-bg"><i class="fa fa-link"></i></a><a href="assets/images/portfolio/grid/5.jpg" class="zoom main-bg" title="Doing it the Chilled Way"><i class="fa fa-search-plus"></i></a>
+                                        </div>
+                                        <img alt="" src="assets/images/portfolio/grid/5.jpg">
+                                    </div>
+                                    <figcaption>
+                                        <div class="port-captions">
+                                            <h4 class="uppercase"><a class="main-color" href="portfolio-single.html">Doing it the Chilled Way</a></h4>
+                                            <p class="description"><a href="#">Design</a>, <a href="#">Development</a></p>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <div class="portfolio-item computers">
+                                <figure>
+                                    <div class="port-img">
+                                        <div class="icon-links">
+                                            <a href="portfolio-single.html" class="link white-bg"><i class="fa fa-link"></i></a><a href="assets/images/portfolio/grid/6.jpg" class="zoom main-bg" title="Take a Ride in a Luxury Car"><i class="fa fa-search-plus"></i></a>
+                                        </div>
+                                        <img alt="" src="assets/images/portfolio/grid/6.jpg">
+                                    </div>
+                                    <figcaption>
+                                        <div class="port-captions">
+                                            <h4 class="uppercase"><a class="main-color" href="portfolio-single.html">Take a Ride in a Luxury Car</a></h4>
+                                            <p class="description"><a href="#">Design</a>, <a href="#">Development</a></p>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
+        
            @include('dprdtd.page.news-sidebar')
     </div>
 </div>
