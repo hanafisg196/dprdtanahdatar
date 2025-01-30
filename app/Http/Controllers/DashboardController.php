ce4c9b4fbbeaffcb3d03b1c262fa863d29efcf10
hanafisg196 <hanafisg196@gmail.com>
2025-01-30 10:08:35 +0700

    sqdsas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\DashboardService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request)
+    protected DashboardService $dashboardService;
+    public function __construct(
+        DashboardService $dashboardService
+
+    ){
+        $this->dashboardService = $dashboardService;
+    }
+    public function index()
     {
-        return view('dashboard.page.dashboard');
+        $data = $this->dashboardService->getViews();
+
+        return view('dashboard.page.dashboard')->with('data', $data);
     }
 }
--- app/Services/Impl/DahsboardServiceImpl.php
+++ app/Services/Impl/DahsboardServiceImpl.php
@@ -1,10 +1,19 @@
 <?php
 namespace App\Services\Impl;
 
+use App\Models\Member;
+use App\Models\News;
+use App\Models\Party;
 use App\Services\DashboardService;
 class  DahsboardServiceImpl implements DashboardService {
     public function getViews(){
-            $hello = "Hello";
-            return $hello;
+            $member = Member::count();
+            $parties = Party::count();
+            $news = News::count();
+            return [
+                'member' => $member,
+                'parties' => $parties,
+                'news' => $news,
+            ];
     }
 }
